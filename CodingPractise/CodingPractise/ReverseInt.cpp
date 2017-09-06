a2fab5b02337a96aba334962345a98a53bb240f8
Dominic Zhang <d.zhang@qsrinternationl.com>
2017-09-06 22:57:46 +1000

    reverse int

---
+++ CodingPractise/CodingPractise/ReverseInt.cpp
@@ -0,0 +1,72 @@
+//
+//  ReverseInt.cpp
+//  CodingPractise
+//
+//  Created by Dominic Zhang on 6/9/17.
+//  Copyright © 2017 Dominic Zhang. All rights reserved.
+//
+
+#include "ReverseInt.hpp"
+#include "gtest/gtest.h"
+#include <stack>
+#include <cmath>
+
+/*
+ Reverse digits of an integer.
+
+ Example1: x = 123, return 321
+ Example2: x = -123, return -321
+
+ Note:
+ The input is assumed to be a 32-bit signed integer. Your function should return 0 when the reversed integer overflows.
+ */
+
+using namespace std;
+
+int reverse(int x) {
+	if (x == 0)
+		return 0;
+	int fabX = fabs(x);
+	int sign = x / fabX;
+	stack<int> numbers;
+	while (fabX > 0) {
+		int remainder = fabX % 10;
+		fabX /= 10;
+		numbers.push(remainder);
+	}
+
+	int result = 0;
+	int i = 0;
+	while (!numbers.empty()) {
+		result += numbers.top() * pow(10, i++);
+		if (result < 0)
+			return 0;
+		numbers.pop();
+	}
+	return result * sign;
+}
+
+TEST(ReverseIntTest, Zero)
+{
+	EXPECT_EQ(0, reverse(0));
+}
+
+TEST(ReverseIntTest, positiveOverflow)
+{
+	EXPECT_EQ(0, reverse(2147483647));
+}
+
+TEST(ReverseIntTest, negativeOverflow)
+{
+	EXPECT_EQ(0, reverse(-2147483647));
+}
+
+TEST(ReversetIntTest, reversePositiveInt)
+{
+	EXPECT_EQ(123, reverse(321));
+}
+
+TEST(ReverseIntTest, reversetNegativeInt)
+{
+	EXPECT_EQ(-123, reverse(-321));
+}
---
+++ CodingPractise/CodingPractise/ReverseInt.hpp
@@ -0,0 +1,14 @@
+//
+//  ReverseInt.hpp
+//  CodingPractise
+//
+//  Created by Dominic Zhang on 6/9/17.
+//  Copyright © 2017 Dominic Zhang. All rights reserved.
+//
+
+#ifndef ReverseInt_hpp
+#define ReverseInt_hpp
+
+#include <stdio.h>
+
+#endif /* ReverseInt_hpp */
